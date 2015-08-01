bb430390da6bca61612a5cbde4118e2d64ce0c31
danniv21 <danniv21@gmail.com>
2015-08-01 04:48:07 -0500

    create a new demo class and update app class

---
+++ example-project-app/src/main/java/org/unmsm/demo01.java
@@ -0,0 +1,40 @@
+package org.unmsm;
+
+public class demo01 {
+	
+	private String param1;
+	private static int flag=0;
+	protected boolean dt;
+	
+	public demo01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getParam1() {
+		return param1;
+	}
+
+	public void setParam1(String param1) {
+		this.param1 = param1;
+	}
+
+	public static int getFlag() {
+		return flag;
+	}
+
+	public static void setFlag(int flag) {
+		demo01.flag = flag;
+	}
+
+	public boolean isDt() {
+		return dt;
+	}
+
+	public void setDt(boolean dt) {
+		this.dt = dt;
+	}
+	
+	
+	
+
+}
